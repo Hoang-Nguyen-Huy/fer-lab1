312122f31c4dfeb333ea2a5e395154a537375a3e
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-09-18 12:47:53 +0700

    feat: 🎸 rating mui

--- src/components/MainContent/OrchidCard.jsx
+++ src/components/MainContent/OrchidCard.jsx
@@ -5,6 +5,7 @@ import CardContent from "@mui/joy/CardContent";
 import CardOverflow from "@mui/joy/CardOverflow";
 import Divider from "@mui/joy/Divider";
 import Typography from "@mui/joy/Typography";
+import { Rating } from "@mui/material";
 
 export default function OrchidCard({ orchid }) {
   return (
@@ -12,10 +13,10 @@ export default function OrchidCard({ orchid }) {
       variant="outlined"
       sx={{
         width: 320,
-        bgcolor: 'black',
-        color: 'white',
-        '&:hover': {
-          boxShadow: '0 0 10px rgba(255, 255, 255, 0.3)',
+        bgcolor: "black",
+        color: "white",
+        "&:hover": {
+          boxShadow: "0 0 10px rgba(255, 255, 255, 0.3)",
         },
       }}
     >
@@ -30,29 +31,37 @@ export default function OrchidCard({ orchid }) {
         </AspectRatio>
       </CardOverflow>
       <CardContent>
-        <Typography level="title-md" sx={{ color: 'white', fontWeight: 'bold', mb: 0.5 }}>{orchid.name}</Typography>
-        <Typography level="body-sm" sx={{ color: 'grey.400', mb: 2 }}>{orchid.origin}</Typography>
+        <Typography
+          level="title-md"
+          sx={{ color: "white", fontWeight: "bold", mb: 0.5 }}
+        >
+          {orchid.name}
+        </Typography>
+        <Typography level="body-sm" sx={{ color: "grey.400", mb: 2 }}>
+          {orchid.origin}
+        </Typography>
       </CardContent>
       <CardOverflow variant="soft" sx={{ bgcolor: "rgba(255, 255, 255, 0.1)" }}>
-        <Divider inset="context" sx={{ bgcolor: 'white' }}/>
+        <Divider inset="context" sx={{ bgcolor: "white" }} />
         <CardContent orientation="horizontal">
-          <Typography
+          {/* <Typography
             level="body-xs"
             textColor="text.secondary"
-            sx={{ 
+            sx={{
               fontWeight: "md",
-              color: 'white' 
+              color: "white",
             }}
           >
             {orchid.rating} rating
-          </Typography>
-          <Divider orientation="vertical" sx={{ color: 'white' }}/>
+          </Typography> */}
+          <Rating name="read-only" value={orchid.rating} readOnly />
+          <Divider orientation="vertical" sx={{ color: "white" }} />
           <Typography
             level="body-xs"
             textColor="text.secondary"
-            sx={{ 
+            sx={{
               fontWeight: "md",
-              color: 'white' 
+              color: "white",
             }}
           >
             {orchid.category}
