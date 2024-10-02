3357a5c0d1b3315a54f9c564eb844c8aec5e403b
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-10-02 10:43:41 +0700

    feat: 🎸 ui contact

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Route, Routes } from "react-router-dom";
 import OrchidDetail from "./components/OrchidDetail";
 import SpecialOrchid from "./components/SpecialOrchid";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,6 +20,7 @@ function App() {
         <Route path='/fer-lab1/natural' element={<SpecialOrchid />} />
         <Route path='/fer-lab1/:id' element={<OrchidDetail />}></Route>
         <Route path='/fer-lab1/about' element={<About />} />
+        <Route path='/fer-lab1/contact' element={<Contact />} />
       </Routes>
       <Footer />
     </>
