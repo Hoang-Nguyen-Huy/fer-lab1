91215a2fafa8fa6f2953d0fec2d61265848110f0
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-10-05 13:28:39 +0700

    refactor: 💡 responsive for home page

--- src/components/MainContent/Content.jsx
+++ src/components/MainContent/Content.jsx
@@ -4,32 +4,32 @@ import { Orchids } from "../../ListOfOrchids";
 import OrchidCard from "./OrchidCard";
 import { useContext } from "react";
 import { ThemeContext } from "../../themes/ThemeContext";
+import { Box } from "@mui/material";
 
 export default function Content() {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <Grid
-      container
-      spacing={{ xs: 2, md: 3, lg: 4 }}
-      // columns={{ xs: 1, sm: 2, md: 3, lg: 4 }}
-      sx={{
-        flexGrow: 1,
-        justifyContent: "center",
-        padding: 2,
-        width: "100%",
-        margin: "0 auto",
-        backgroundColor: theme.mainContent.backgroundColor,
-        paddingTop: '36px'
-      }}
-    >
-      {Orchids.map((orchid) => {
-        return (
-          <Grid key={orchid.Id} size={{ xs: 1, sm: 3, md: 4 }}>
-            <OrchidCard key={orchid.Id} orchid={orchid} />
-          </Grid>
-        );
-      })}
-    </Grid>
+    <Box sx={{ width: "100%" }}>
+      <Grid
+        container
+        spacing={{ xs: 3, md: 4, lg: 4 }}
+        sx={{
+          padding: 2,
+          width: "100%",
+          margin: "0 auto",
+          backgroundColor: theme.mainContent.backgroundColor,
+          paddingTop: "36px",
+        }}
+      >
+        {Orchids.map((orchid) => {
+          return (
+            <Grid key={orchid.Id} item xs={12} sm={6} md={4}>
+              <OrchidCard key={orchid.Id} orchid={orchid} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
   );
 }
