682ed207802714e12a89183168b5ab2cef08559c
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-10-22 13:22:42 +0700

    feat: 🎸 auth context

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import { ThemeProvider } from "./themes/ThemeContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </AuthContextProvider>
   </StrictMode>
 );
