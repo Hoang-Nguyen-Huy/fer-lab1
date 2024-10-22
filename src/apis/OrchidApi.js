33859245c91b08a4a1aed31c759ad3b6cdaf6af1
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-10-22 20:57:34 +0700

    feat: 🎸 get special orchid api

--- src/apis/OrchidApi.js
+++ src/apis/OrchidApi.js
@@ -2,7 +2,20 @@ const baseUrl = `https://670f54e33e71518616575e20.mockapi.io/orchids-lab`;
 
 export const getAllOrchids = async () => {
   try {
-    const response = await fetch(`${baseUrl}?sortBy=Id&order=desc`);
+    const response = await fetch(`${baseUrl}`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching orchids:", error);
+    throw error;
+  }
+};
+
+export const getSpecialOrchids = async () => {
+  try {
+    const response = await fetch(`${baseUrl}?isSpecial=true`);
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
