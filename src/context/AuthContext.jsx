5b839b8e7b04f1b4809b2614d2e6792f3ff46938
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-10-22 13:22:42 +0700

    feat: 🎸 auth context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { auth } from "../firebase";
+
+const AuthContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+  const logOut = () => {
+    signOut(auth);
+  };
+  useEffect(() => {
+    const unSubcribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unSubcribe();
+    };
+  }, []);
+  return (
+    <AuthContext.Provider value={{ googleSignIn, logOut, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const UserAuth = () => {
+  return useContext(AuthContext);
+};
