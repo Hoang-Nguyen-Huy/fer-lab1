59623f2e818bfffae8697999dab7c40143652ec9
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-10-22 22:21:37 +0700

    feat: 🎸 ready to delete api url

---
+++ src/apis/OrchidsApi.js
@@ -0,0 +1,41 @@
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+export const getAllOrchids = async () => {
+  try {
+    const response = await fetch(`${baseUrl}`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching orchids:", error);
+    throw error;
+  }
+};
+
+export const getSpecialOrchids = async () => {
+  try {
+    const response = await fetch(`${baseUrl}?isSpecial=true`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching orchids:", error);
+    throw error;
+  }
+};
+
+export const getOrchidById = async (id) => {
+  try {
+    const response = await fetch(`${baseUrl}?Id=${id}`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data[0];
+  } catch (error) {
+    console.error("Error fetching orchids:", error);
+    throw error;
+  }
+};
